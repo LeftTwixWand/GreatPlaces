4626cd85c2182a867826b9e8508d0f1e5e3f102d
Vladyslav <lefttwixwand@gmail.com>
2021-09-02 22:54:04 +0300

    feat: Inserting data into a database

---
+++ lib/helpers/db_helper.dart
@@ -0,0 +1,19 @@
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as path;
+
+class DBHelper {
+  static Future<void> insert(String table, Map<String, Object> data) async {
+    final dbPath = await sql.getDatabasesPath();
+    final database = await sql.openDatabase(path.join(dbPath, 'places.db'),
+        onCreate: _onDatabaseCreating);
+
+    database.insert(table, data,
+        conflictAlgorithm: sql.ConflictAlgorithm.replace);
+  }
+
+  static Future<void> _onDatabaseCreating(sql.Database db, int version) {
+    return db.execute(
+      'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT)',
+    );
+  }
+}
--- lib/providers/places_provider.dart
+++ lib/providers/places_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
+import 'package:great_places/helpers/db_helper.dart';
 import '../models/place.dart';
 
 class PlacesProvider with ChangeNotifier {
@@ -9,14 +10,15 @@ class PlacesProvider with ChangeNotifier {
   List<Place> get items => [..._items];
 
   void addPlace(String title, File image) {
-    final newPlace = Place(
-      DateTime.now().toString(),
-      title,
-      null,
-      image,
-    );
+    final newPlace = Place(DateTime.now().toString(), title, null, image);
 
     _items.add(newPlace);
     notifyListeners();
+
+    DBHelper.insert('places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
   }
 }
